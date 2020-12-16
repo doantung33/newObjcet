ee23bd29125d0fc59d40c9b3dee839f878a0156c
doan tung <doantung2401@gmail.com>
2020-12-16 15:28:06 +0700

    new

---
+++ src/Objcet.java
@@ -0,0 +1,18 @@
+public class Objcet {
+
+    public static void main(String[] args) {
+        int a = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b= true;
+        char c= 'a';
+        String s= " HD";
+        System.out.println("a= "+ a);
+        System.out.println("f= "+ f);
+        System.out.println("d= "+d);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+        System.out.println("s= "+s);
+    }
+
+}
